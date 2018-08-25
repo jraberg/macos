cffa2efe67f249a14ee91f6556b4dc5ffcbef087
jraberg <jonas.raberg@gmail.com>
2018-08-25 17:16:49 +0200

    Fixed output.

--- base-intall.sh
+++ base-intall.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Install xCode ..."
+echo "==>Install xCode ..."
 xcode-select --install
 
-echo "Install Homebrew ..."
+echo "==>Install Homebrew ..."
 if [[ ! -x "/usr/local/bin/brew" ]]; then
    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
    echo "homebrew allready installed."
 fi
 
-echo "Install command line tools ..."
+echo "==>Install command line tools ..."
 brew install wget
 brew install tree
 brew install git
@@ -34,26 +34,26 @@ brew install watch
 brew install watchman
 
 
-echo "Install Homebrew services ..."
+echo "==>Install Homebrew services ..."
 brew tap homebrew/services
 
-echo "Install Postgres SQL ..."
+echo "==>Install Postgres SQL ..."
 brew install postgresql
 
-echo "Install MySQL ..."
+echo "==>Install MySQL ..."
 brew install mysql
 
-echo "Install Golang ..."
+echo "==>Install Golang ..."
 brew install go
 
-echo "Installing Python ..."
+echo "==>Installing Python ..."
 brew install python
 pip3 install --upgrade setuptools
 pip3 install --upgrade pip
 pip3 install virtualenv
 pip3 install virtualenvwrapper
 
-echo "Install node.js and tools ..."
+echo "==>Install node.js and tools ..."
 brew install node
 node -v
 brew install nvm
@@ -63,21 +63,21 @@ npm install -g typescript
 brew install yarn
 brew install prettier
 
-echo "Install Circle CI tools ..."
+echo "==>Install Circle CI tools ..."
 brew install circleci
 
-echo "Install AWS cli client ..."
+echo "==>Install AWS cli client ..."
 brew install awscli
 
-echo "Install lastpass cli ..."
+echo "==>Install lastpass cli ..."
 brew install lastpass-cli --with-pinentry --with-doc
 
-echo "Install Ansible ..."
+echo "==>Install Ansible ..."
 brew install ansible
 brew install ansible-lint
 
 # Install zsh theme
-echo "Install zsh extention oh-my-zsh with spaceship prompt ..."
+echo "==>Install zsh extention oh-my-zsh with spaceship prompt ..."
 bash <<EOF
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 exit
@@ -88,7 +88,7 @@ brew tap caskroom/fonts
 brew cask install font-fira-code
 
 # Development
-echo "Install development tools ..."
+echo "==>Install development tools ..."
 brew cask install --appdir="/Applications" iterm2
 brew cask install --appdir="/Applications" visual-studio-code
 brew cask install --appdir="/Applications" webstorm
@@ -104,7 +104,7 @@ brew cask install --appdir="/Applications" postman
 
 
 # Core Functionality
-echo "Install base applications ..."
+echo "==>Install base applications ..."
 brew cask install --appdir="/Applications" alfred
 brew cask install --appdir="/Applications" dropbox
 brew cask install --appdir="~/Applications" transmit
